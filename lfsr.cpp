b9801bb8480275cd3577907d8eccd1b6bb41f1a9
Roy Chen <77559865+roy-chen57@users.noreply.github.com>
2023-06-06 15:56:08 -0700

    Add files via upload
    Fix the maximal primitive polynomial for 8 bit length Lfsrs' and change the generate method to generate numbers with the same bit length as the bitlength member variable.

--- lfsr.cpp
+++ lfsr.cpp
@@ -1,10 +1,12 @@
+#pragma once 
 #include <iostream>
 #include "lfsr.h"
 
 // Default Constructor
 Lfsr::Lfsr() : bitstr{ Prng::seed }, bitlength{ find_bitlength(bitstr) }{};
-// Constructor
+// Constructors
 Lfsr::Lfsr(int seed) : bitstr{ seed }, bitlength{ find_bitlength(seed) }{};
+Lfsr::Lfsr(int seed, int bitlength) : bitstr{ seed }, bitlength{ bitlength }{};
 
 /*
 	find_bitlength(num) takes an integer num and returns the length of 
@@ -27,8 +29,6 @@ int Lfsr::get_bitstr() { return bitstr; }
 // void -> int
 int Lfsr::get_bitlength() { return bitlength; }
 
-
-
 /*
 	shift(num) right-shifts the bitstr member variable by one positon(loses the rightmost bit)
 		and appends a new bit to the left of bitstr and returns the bit popped off the bitstr
@@ -37,11 +37,15 @@ int Lfsr::get_bitlength() { return bitlength; }
 */
 int Lfsr::shift() {
 	int state = (bitstr & 1); //last bit of num
-	bitstr = bitstr >> 1;
 	// feedback function to determine the new bit
-	int feedback = state ^ ((bitstr >> 2) & 1);
+	// (guarantees a maximal cycle only for 8 bit seeds
+	// x^8 + x^6 + x^5 + x + 1
+	int feedback = state ^ ((bitstr >> 2) & 1) ^ ((bitstr >> 3) & 1) ^ ((bitstr >> 6) & 1);
+
+	bitstr = bitstr >> 1;
 	// append feedback bit to start of the bit string 
 	bitstr += (feedback << (bitlength - 1));
+
 	return state;
 }
 
@@ -49,7 +53,7 @@ int Lfsr::shift() {
 // void -> int
 int Lfsr::generate() {
 	int result = 0;
-	for (int i = 0; i < bitlength/2; ++i) {
+	for (int i = 0; i < bitlength; ++i) {
 		result += (shift() << i);
 	}
 	return result;
